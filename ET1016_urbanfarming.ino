23568c010c10f7bd8a4540b815b8433f42db9214
shaomin08 <lsmmin02.25@ichat.sp.edu.sg>
2025-07-24 15:47:22 +0800

    Create ET1016_urbanfarming.ino

---
+++ ET1016_urbanfarming.ino
@@ -0,0 +1,15 @@
+// Title: URBAN FARMING
+// Name: ShaoMin, Daniel
+// Admin no: P2515971, P2515843
+// Class: DCEP/FT/1A/09
+// Description: Control of water (watering the plants - the blue led light will on when the sprinkler is on, along with the motor sensor that will move in a systematic manner to water the plant after a set about of time using the display), ac (optimal temp for the plants - when the temperature detected exceeds a certain degree, the ac will auto switch on to allow the farm to cool down to the optimal temp for the plant, the red led will on when the temp is too high)
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
